dd54da8809a6708c65f4c522d9219a3b59e3512f
Jiya Makhija <jmakhija@ucsd.edu>
2025-05-15 14:00:48 -0700

    Adding cambridge_route

--- map.js
+++ map.js
@@ -13,6 +13,7 @@ const map = new mapboxgl.Map({
   });
 
   map.on('load', async () => {
+    // Boston
     map.addSource('boston_route', {
       type: 'geojson',
       data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
@@ -28,6 +29,21 @@ const map = new mapboxgl.Map({
         'line-opacity': 0.4
       },
     });
+
+    // Cambridge
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeLaneStyle,
+    });
+
+
   
     console.log("Bike lanes loaded!");
   });
