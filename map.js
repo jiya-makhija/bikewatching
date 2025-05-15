4e7e798a606502e5916e6941d636a5eaa0a08418
Jiya Makhija <jmakhija@ucsd.edu>
2025-05-15 13:56:01 -0700

    Adding data

--- map.js
+++ map.js
@@ -11,3 +11,23 @@ const map = new mapboxgl.Map({
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18, // Maximum allowed zoom
   });
+
+  map.on('load', async () => {
+    map.addSource('boston_route', {
+      type: 'geojson',
+      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+  
+    map.addLayer({
+      id: 'bike-lanes',
+      type: 'line',
+      source: 'boston_route',
+      paint: {
+        'line-color': '#FF00FF',  // magenta
+        'line-width': 5,
+        'line-opacity': 0.6
+      },
+    });
+  
+    console.log("Bike lanes loaded!");
+  });
